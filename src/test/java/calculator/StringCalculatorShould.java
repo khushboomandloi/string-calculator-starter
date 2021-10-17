b3207f1547eaf2d9146a3a88da422eef44eb1b11
khushboomandloi <92640914+khushboomandloi@users.noreply.github.com>
2021-10-17 15:42:30 +0530

    test case added

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -23,4 +23,16 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("1,2"));
     }
+
+    @Test
+    public void testThreeNumbers(){
+        StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(6, stringCalculator.add("1,2,3"));
+    }
+
+    @Test
+    public void testNewLine(){
+        StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(6, stringCalculator.add("1\n2,3"));
+    }
 }
