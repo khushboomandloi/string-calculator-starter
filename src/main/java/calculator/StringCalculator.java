eb2161c3258f02da6abc70ddf21388bbb2e53c9e
khushboomandloi <92640914+khushboomandloi@users.noreply.github.com>
2021-10-17 14:20:33 +0530

    Add method added

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,27 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
-    }
+        if(input.equals("")){
+			return 0;
+		}
+		else{
+			String delimiter = ",";
+			if(input.matches("//(.)\n(.)")){
+				delimiter = Character.toString(input.charAt(2));
+				input = input.substring(4);
+			}
 
+			String numList[] = splitNumbers(input, delimiter + "|\n");
+			return sum(numList);
+        }
+    }
+    private int sum(String[] numList) {
+		return 0;
+	}
+	private String[] splitNumbers(String input, String string) {
+		return null;
+	}
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+    }
 }
